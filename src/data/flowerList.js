24d7d7502123bc3969e22d80aaecb2fc8d835591
Bartek Brychcy <bbrychcy@onet.eu>
2021-04-30 19:39:26 +0200

    Add  data to FlowerList.js
    add id, name, price | add description to first object rest in the future

---
+++ src/data/flowerList.js
@@ -0,0 +1,196 @@
+export const flowerList = [
+    {
+        id: 0,
+        name: 'aksamitka',
+        price: 2,
+        description: '',
+
+    },
+    {
+        id: 1,
+        name: 'ampla',
+        price: 25,
+
+    },
+    {
+        id: 2,
+        name: 'bakopa',
+        price: 4,
+
+    },
+    {
+        id: 3,
+        name: 'bakopa wisząca',
+        price: 20,
+
+    },
+    {
+        id: 4,
+        name: 'batat',
+        price: 6,
+
+    },
+    {
+        id: 5,
+        name: 'begonia standard',
+        price: 6,
+
+    },
+    {
+        id: 6,
+        name: 'begonia dragon',
+        price: 7,
+
+    }, {
+        id: 7,
+        name: 'begonia solewia',
+        price: 7,
+
+    }, {
+        id: 8,
+        name: 'begonia stale kwitnąca',
+        price: 2,
+
+    }, {
+        id: 9,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 10,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 11,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 12,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 13,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 14,
+        name: 'begonia',
+        price: 6,
+
+    }, {
+        id: 15,
+        name: 'begonia',
+        price: 6,
+
+    },
+    {
+        id: 16,
+        name: 'dalia',
+        price: 6,
+
+    },
+    {
+        id: 17,
+        name: 'dalia',
+        price: 6,
+
+    }, {
+        id: 18,
+        name: 'fuksja',
+        price: 5,
+
+    },
+    {
+        id: 19,
+        name: 'heliotrop',
+        price: 6,
+
+    }, {
+        id: 20,
+        name: 'kacanka',
+        price: 4,
+
+    },
+    {
+        id: 21,
+        name: 'koci ogon',
+        price: 6,
+
+    },
+    {
+        id: 22,
+        name: 'koci ogon wiszący',
+        price: 25,
+
+    },
+    {
+        id: 23,
+        name: 'komarzyca',
+        price: 4,
+
+    },
+    {
+        id: 24,
+        name: 'lewizja',
+        price: 6,
+
+    },
+    {
+        id: 25,
+        name: 'lobelia',
+        price: 5,
+
+    },
+    {
+        id: 26,
+        name: 'milion dzwoneczków',
+        price: 5,
+
+    },
+    {
+        id: 27,
+        name: 'niecierpek',
+        price: 5,
+
+    },
+    {
+        id: 28,
+        name: 'pelargonia',
+        price: 5,
+
+    },
+    {
+        id: 29,
+        name: 'pelargonia bordo',
+        price: 6,
+
+    },
+    {
+        id: 30,
+        name: 'stokrotka afrykańska',
+        price: 6,
+
+    },
+    {
+        id: 31,
+        name: 'sunvitalia',
+        price: 4,
+
+    }, {
+        id: 32,
+        name: 'surfinia',
+        price: 4,
+
+    },
+    {
+        id: 33,
+        name: 'wilczomlecz',
+        price: 5,
+
+    },
+];
+
