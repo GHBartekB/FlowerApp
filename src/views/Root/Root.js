7e1b226740f5253d5170d21d8ef241e9f446af43
Bartek Brychcy <bbrychcy@onet.eu>
2021-04-30 19:06:31 +0200

    Setup project
    change App.js to Root.js
    add views directory
    add data directory
    install node-sass and sass-loader

---
+++ src/views/Root/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  state = {}
+
+  render() {
+
+    return (
+      <>
+        <h2>test</h2>
+      </>
+    )
+  }
+
+}
+
+export default App;
