fd34c3825d39b3379cd41b95643d1afbe43288bf
Bartek Brychcy <bbrychcy@onet.eu>
2021-05-02 15:01:33 +0200

    change data values in state | OrderView.js && change data values in FlowerList.js

--- src/data/FlowerList.js
+++ src/data/FlowerList.js
@@ -1,181 +1,182 @@
 export const FlowerList = [
     {
         id: 1,
-        aksCount: 0,
         name: 'aksamitka',
+        stateName: 'aks',
         price: 2,
         description: '',
 
     },
     {
         id: 2,
-        ampCount: 0,
         name: 'ampla',
+        stateName: 'amp',
         price: 25,
 
     },
     {
         id: 3,
-        bakCount: 0,
         name: 'bakopa',
+        stateName: 'bak',
         price: 4,
 
     },
     {
         id: 4,
-        bakWCount: 0,
         name: 'bakopa wisząca',
+        stateName: 'bakWis',
         price: 20,
 
     },
     {
         id: 5,
-        batCount: 0,
         name: 'batat',
+        stateName: 'bat',
         price: 6,
 
     },
     {
         id: 6,
-        begStandCount: 0,
-        name: 'begonia standard',
+        name: 'begSta',
+        stateName: 'begoniaStandard',
         price: 6,
 
     },
     {
         id: 7,
-        begDragCount: 0,
         name: 'begonia dragon',
+        stateName: 'begDra',
         price: 7,
 
     }, {
         id: 8,
-        begSolCount: 0,
         name: 'begonia solewia',
+        stateName: 'begSol',
         price: 7,
 
     }, {
         id: 9,
-        begStalKwiCount: 0,
         name: 'begonia stale kwitnąca',
+        stateName: 'begStaKwi',
         price: 2,
 
     },
 
     {
         id: 10,
-        dalCount: 0,
         name: 'dalia',
+        stateName: 'dal',
         price: 6,
 
     },
     {
         id: 11,
-        fukCount: 0,
         name: 'fuksja',
+        stateName: 'fuk',
         price: 5,
 
     },
     {
         id: 12,
-        helCount: 0,
         name: 'heliotrop',
+        stateName: 'hel',
         price: 6,
 
     }, {
         id: 13,
-        kacCount: 0,
         name: 'kacanka',
+        stateName: 'kac',
         price: 4,
 
     },
     {
         id: 14,
-        kocCount: 0,
         name: 'koci ogon',
+        stateName: 'kocOgo',
         price: 6,
 
     },
     {
         id: 15,
-        kocWisCount: 0,
         name: 'koci ogon wiszący',
+        stateName: 'kocOgoWis',
         price: 25,
 
     },
     {
         id: 16,
-        komCount: 0,
         name: 'komarzyca',
+        stateName: 'kom',
         price: 4,
 
     },
     {
         id: 17,
-        lewCount: 0,
         name: 'lewizja',
+        stateName: 'lew',
         price: 6,
 
     },
     {
         id: 18,
-        lobCount: 0,
         name: 'lobelia',
+        stateName: 'lob',
         price: 5,
 
     },
     {
         id: 19,
-        milCount: 0,
         name: 'milion dzwoneczków',
+        stateName: 'mil',
         price: 5,
 
     },
     {
         id: 20,
-        niecCount: 0,
         name: 'niecierpek',
+        stateName: 'nie',
         price: 5,
 
     },
     {
         id: 21,
-        pelCount: 0,
         name: 'pelargonia',
+        stateName: 'pel',
         price: 5,
 
     },
     {
         id: 22,
-        pelBorCount: 0,
         name: 'pelargonia bordo',
+        stateName: 'pelBor',
         price: 6,
 
     },
     {
         id: 23,
-        stoCount: 0,
         name: 'stokrotka afrykańska',
+        stateName: 'stoAfr',
         price: 6,
 
     },
     {
         id: 24,
-        sunCount: 0,
         name: 'sunvitalia',
+        stateName: 'sun',
         price: 4,
 
     }, {
         id: 25,
         surfCount: 0,
         name: 'surfinia',
+        stateName: 'sur',
         price: 4,
 
     },
     {
         id: 26,
-        wilCount: 0,
         name: 'wilczomlecz',
+        stateName: 'wil',
         price: 5,
 
     },
--- src/views/OrderView/OrderView.js
+++ src/views/OrderView/OrderView.js
@@ -2,38 +2,37 @@ import React from 'react';
 import styles from './OrderView.module.scss';
 import { FlowerList } from '../../data/FlowerList.js';
 
-const productList = [...FlowerList];
-
 class OrderView extends React.Component {
 
     state = {
         sum: 0,
-        [productList[0].name]: productList[0].aksCount,
-        [productList[1].name]: productList[1].ampCount,
-        [productList[2].name]: productList[2].bakCount,
-        [productList[3].name]: productList[3].bakWCount,
-        [productList[4].name]: productList[4].batCount,
-        [productList[5].name]: productList[5].begStandCount,
-        [productList[6].name]: productList[6].begDragCount,
-        [productList[7].name]: productList[7].begSolCount,
-        [productList[8].name]: productList[8].begStalKwiCount,
-        [productList[9].name]: productList[9].dalCount,
-        [productList[10].name]: productList[10].fukCount,
-        [productList[11].name]: productList[11].helCount,
-        [productList[12].name]: productList[12].kacCount,
-        [productList[13].name]: productList[13].kocCount,
-        [productList[14].name]: productList[14].kocWisCount,
-        [productList[15].name]: productList[15].komCount,
-        [productList[16].name]: productList[16].lewCount,
-        [productList[17].name]: productList[17].lobCount,
-        [productList[18].name]: productList[18].milCount,
-        [productList[19].name]: productList[19].niecCount,
-        [productList[20].name]: productList[20].pelCount,
-        [productList[21].name]: productList[21].pelBorCount,
-        [productList[22].name]: productList[22].stoCount,
-        [productList[23].name]: productList[23].sunCount,
-        [productList[24].name]: productList[24].surfCount,
-        [productList[25].name]: productList[25].wilCount,
+        aks: 0,
+        amp: 0,
+        bak: 0,
+        bakWis: 0,
+        bat: 0,
+        begSta: 0,
+        begDra: 0,
+        begSol: 0,
+        begStaKwi: 0,
+        dal: 0,
+        fuk: 0,
+        hel: 0,
+        kac: 0,
+        kocOgo: 0,
+        kocOgoWis: 0,
+        kom: 0,
+        lew: 0,
+        lob: 0,
+        mil: 0,
+        nie: 0,
+        pel: 0,
+        pelBor: 0,
+        stoAfr: 0,
+        sun: 0,
+        sur: 0,
+        wil: 0,
+
 
     }
 
@@ -67,9 +66,9 @@ class OrderView extends React.Component {
 
                 <div key={product.id} className={styles.item}>
                     <span>{product.name}</span>
-                    <button name={product.name} onClick={() => this.handleSubstractBtn(product.price, product.name)} className={styles.btn}>-</button>
-                    <button name={product.name} onClick={() => this.handleAddBtn(product.price, product.name)} className={styles.btn}>+</button>
-                    <span>{this.state.[product.name]}</span>
+                    <button name={product.name} onClick={() => this.handleSubstractBtn(product.price, product.stateName)} className={styles.btn}>-</button>
+                    <button name={product.name} onClick={() => this.handleAddBtn(product.price, product.stateName)} className={styles.btn}>+</button>
+                    <span>{this.state.[product.stateName]}</span>
                     <span>szt.</span>
                 </div>
             )
