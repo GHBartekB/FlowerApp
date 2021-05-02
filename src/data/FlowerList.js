a36b869bfc6932f39360eca325de15a9a9de4bc8
Bartek Brychcy <bbrychcy@onet.eu>
2021-05-02 10:23:00 +0200

    Change FlowerList data (change id && add countName) && add new values to state in OrderView.js

--- src/data/FlowerList.js
+++ src/data/FlowerList.js
@@ -1,154 +1,180 @@
 export const FlowerList = [
     {
-        id: 0,
+        id: 1,
+        countName: 'aksCount',
         name: 'aksamitka',
         price: 2,
         description: '',
 
     },
     {
-        id: 1,
+        id: 2,
+        countName: 'ampCount',
         name: 'ampla',
         price: 25,
 
     },
     {
-        id: 2,
+        id: 3,
+        countName: 'bakCount',
         name: 'bakopa',
         price: 4,
 
     },
     {
-        id: 3,
+        id: 4,
+        countName: 'bakWCount',
         name: 'bakopa wisząca',
         price: 20,
 
     },
     {
-        id: 4,
+        id: 5,
+        countName: 'batCount',
         name: 'batat',
         price: 6,
 
     },
     {
-        id: 5,
+        id: 6,
+        countName: 'begStandCount',
         name: 'begonia standard',
         price: 6,
 
     },
     {
-        id: 6,
+        id: 7,
+        countName: 'begDragCount',
         name: 'begonia dragon',
         price: 7,
 
     }, {
-        id: 7,
+        id: 8,
+        countName: 'begSolCount',
         name: 'begonia solewia',
         price: 7,
 
     }, {
-        id: 8,
+        id: 9,
+        countName: 'begStalKwiCount',
         name: 'begonia stale kwitnąca',
         price: 2,
 
     },
 
     {
-        id: 9,
+        id: 10,
+        countName: 'dalCount',
         name: 'dalia',
         price: 6,
 
     },
     {
-        id: 10,
+        id: 11,
+        countName: 'fukCount',
         name: 'fuksja',
         price: 5,
 
     },
     {
-        id: 11,
+        id: 12,
+        countName: 'helCount',
         name: 'heliotrop',
         price: 6,
 
     }, {
-        id: 12,
+        id: 13,
+        countName: 'kacCount',
         name: 'kacanka',
         price: 4,
 
     },
     {
-        id: 13,
+        id: 14,
+        countName: 'kocCount',
         name: 'koci ogon',
         price: 6,
 
     },
     {
-        id: 14,
+        id: 15,
+        countName: 'kocWisCount',
         name: 'koci ogon wiszący',
         price: 25,
 
     },
     {
-        id: 15,
+        id: 16,
+        countName: 'komCount',
         name: 'komarzyca',
         price: 4,
 
     },
     {
-        id: 16,
+        id: 17,
+        countName: 'lewCount',
         name: 'lewizja',
         price: 6,
 
     },
     {
-        id: 17,
+        id: 18,
+        countName: 'lobCount',
         name: 'lobelia',
         price: 5,
 
     },
     {
-        id: 18,
+        id: 19,
+        countName: 'milCount',
         name: 'milion dzwoneczków',
         price: 5,
 
     },
     {
-        id: 19,
+        id: 20,
+        countName: 'niecCount',
         name: 'niecierpek',
         price: 5,
 
     },
     {
-        id: 20,
+        id: 21,
+        countName: 'pelCount',
         name: 'pelargonia',
         price: 5,
 
     },
     {
-        id: 21,
+        id: 22,
+        countName: 'pelBorCount',
         name: 'pelargonia bordo',
         price: 6,
 
     },
     {
-        id: 22,
+        id: 23,
+        countName: 'stoCount',
         name: 'stokrotka afrykańska',
         price: 6,
 
     },
     {
-        id: 23,
+        id: 24,
+        countName: 'sunCount',
         name: 'sunvitalia',
         price: 4,
 
     }, {
-        id: 24,
+        id: 25,
+        countName: 'surfCount',
         name: 'surfinia',
         price: 4,
 
     },
     {
-        id: 25,
+        id: 26,
+        countName: 'wilCount',
         name: 'wilczomlecz',
         price: 5,
 
--- src/views/OrderView/OrderView.js
+++ src/views/OrderView/OrderView.js
@@ -1,33 +1,84 @@
 import React from 'react';
 import styles from './OrderView.module.scss';
 
-const OrderView = () => {
+class OrderView extends React.Component {
 
-    return (
-        <>
-            <div className={styles.item}>
-                <span>Begonia stale kwitnąca</span>
-                <button className={styles.btn}>-</button>
-                <button className={styles.btn}>+</button>
-                <span>10</span>
-                <span>szt.</span>
-            </div>
-            <div className={styles.item}>
-                <span>Begonia stale kwitnąca</span>
-                <button className={styles.btn}>-</button>
-                <button className={styles.btn}>+</button>
-                <span>10</span>
-                <span>szt.</span>
-            </div>
-            <div className={styles.item}>
-                <span>Begonia stale kwitnąca</span>
-                <button className={styles.btn}>-</button>
-                <button className={styles.btn}>+</button>
-                <span>10</span>
-                <span>szt.</span>
-            </div>
-        </>
-    )
+    state = {
+        sum: 0,
+        aksCount: 0,
+        ampCount: 0,
+        bakCount: 0,
+        bakWCount: 0,
+        batCount: 0,
+        begStandCount: 0,
+        begDragCount: 0,
+        begSolCount: 0,
+        begStalKwiCount: 0,
+        dalCount: 0,
+        fukCount: 0,
+        helCount: 0,
+        kacCount: 0,
+        kocCount: 0,
+        kocWisCount: 0,
+        komCount: 0,
+        lewCount: 0,
+        lobCount: 0,
+        milCount: 0,
+        niecCount: 0,
+        pelCount: 0,
+        pelBorCount: 0,
+        stoCount: 0,
+        sunCount: 0,
+        surCount: 0,
+        wilCount: 0,
+
+    }
+
+    handleAddBtn = (count) => {
+        const productPrice = parseInt(count);
+        this.setState(prevState => ({
+            sum: prevState.sum + productPrice,
+        }))
+    }
+
+    handleSubstractBtn = (count) => {
+        const productPrice = parseInt(count);
+        this.setState(prevState => ({
+            sum: prevState.sum - productPrice,
+        }))
+    }
+
+    render() {
+
+        return (
+            <>
+                <div className={styles.item}>
+                    <span>Begonia stale kwitnąca</span>
+                    <button onClick={() => this.handleSubstractBtn('5')} className={styles.btn}>-</button>
+                    <button onClick={() => this.handleAddBtn('5')} className={styles.btn}>+</button>
+                    <span>{this.state.aksCount}</span>
+                    <span>szt.</span>
+                </div>
+                <div className={styles.item}>
+                    <span>Begonia stale kwitnąca</span>
+                    <button className={styles.btn}>-</button>
+                    <button className={styles.btn}>+</button>
+                    <span>10</span>
+                    <span>szt.</span>
+                </div>
+                <div className={styles.item}>
+                    <span>Begonia stale kwitnąca</span>
+                    <button className={styles.btn}>-</button>
+                    <button className={styles.btn}>+</button>
+                    <span>10</span>
+                    <span>szt.</span>
+                </div>
+                <div>
+                    <h2>Do zapłaty: {this.state.sum} zł</h2>
+                </div>
+            </>
+        )
+    }
 }
 
 export default OrderView;
